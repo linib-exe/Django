943b47958ea610731ab0996f69396ce968c32314
Binilraj Adhikari <linib-exe>
2023-08-31 11:31:39 +0545

    Form Added

--- Login and Register/authenticationapp/views.py
+++ Login and Register/authenticationapp/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your views here.
-def home(request):
-    return render(request,'login.html')
 
 def login_view(request):
     if request.method == 'POST':
