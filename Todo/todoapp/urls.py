816882e44b920e1acb568a7f5dab52c4cad4060f
Binilraj Adhikari <linib-exe>
2023-08-31 19:13:36 +0545

    CRUD Full

--- Todo/todoapp/urls.py
+++ Todo/todoapp/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('create/',views.create,name='create'),
     path('todo_list/',views.retreive,name='todo_list'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>/',views.delete,name='delete'),
+
 ]
--- Todo/todoapp/views.py
+++ Todo/todoapp/views.py
@@ -11,9 +11,24 @@ def create(request):
             return redirect('todo_list')
     else:
         form = TodoForm()
-    
     return render(request, 'todo_form.html', {'form': form})
-
+    
 def retreive(request):
     todos = TodoItem.objects.all()
     return render(request, 'todo_list.html', {'todos': todos})
+
+def update(request, id):
+    todo = TodoItem.objects.get(pk=id)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo_form.html', {'form': form})
+
+def delete(request,id):
+    todo = TodoItem.objects.get(pk=id)
+    todo.delete()
+    return redirect('/todo_list/')
