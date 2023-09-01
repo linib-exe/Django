810820a72c6604ee8a888625bc90a3b22b489f14
Binilraj Adhikari <linib-exe>
2023-09-01 06:26:03 +0545

    CRUD Updated

--- Todo/todoapp/urls.py
+++ Todo/todoapp/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/',views.create,name='create'),
+    path('todo_list/create/',views.create,name='create'),
     path('todo_list/',views.retreive,name='todo_list'),
-    path('update/<int:id>/',views.update,name='update'),
-    path('delete/<int:id>/',views.delete,name='delete'),
+    path('todo_list/update/<int:id>/',views.update,name='update'),
+    path('todo_list/delete/<int:id>/',views.delete,name='delete'),
 
 ]
